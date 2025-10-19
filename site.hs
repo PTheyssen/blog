4b88807f6d97a1c7ea41b2d6085e166646d90620
Philipp Theyssen <p.theyssen@gmail.com>
2025-10-19 18:41:59 +0200

    First changes (removing index template)

--- site.hs
+++ site.hs
@@ -89,7 +89,7 @@ main = hakyllWith config $ do
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/index-template.html" indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
